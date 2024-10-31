01aec3b5e6e796f2cda4d3597ed9b7d3f5c5fe8c
Dresden Michael Sweet <dsweet797@student.fuhsd.org>
2024-10-31 15:44:30 -0700

    OUT OF ALPHA!
    It's finally out of Alpha! Things are looking up!

--- Real-ClockJ/src/realpackage/RealClock.java
+++ Real-ClockJ/src/realpackage/RealClock.java
@@ -1,20 +1,18 @@
 package realpackage;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.*;
-
 ///TimeZone TimeZ = TimeZone.getTimeZone("America/Los_Angeles");
 
 public class RealClock {
 
-	public void draw(Graphics g, int xWidth, int yHeight) {
-		
-		int xCenter = xWidth/2;
-		int yCenter = yHeight/2;
-		
-		g.setColor(Color.green);
-		g.fillRect(10, 10, 150, 75);
+	float TimeMillis = System.currentTimeMillis();
 	
-	}	
+	float TimeSecs = TimeMillis/1000;
+	float TimeMins = TimeSecs/60;
+	float TimeHour = TimeSecs/60;
+	
+	
+	
+	
+	
+	
 }
--- Real-ClockJ/src/realpackage/RealClockPanel.java
+++ Real-ClockJ/src/realpackage/RealClockPanel.java
@@ -11,16 +11,28 @@ public class RealClockPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		setBackground(Color.white);
-		}
+		
+		int xWidth = getWidth();
+		int yHeight = getHeight();
+		
+		int xCenter = xWidth/2;
+		int yCenter = yHeight/2;
+		
+		g.setColor(Color.black);
+		g.drawRect(xCenter - 200, yCenter - 125, 400, 250);
+///		g.drawString(null, xCenter - 100, yCenter - 75);
+	
+}
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Time Display");
-		window.setBounds(300, 300, 200, 200);
+		window.setBounds(0, 0, 800, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		RealClockPanel timeP = new RealClockPanel();
 		Container c = window.getContentPane();
 		c.add(timeP);
 		window.setVisible(true);
+		window.setResizable(false);
 	}
 	
 }
